542567bb8ceb7e777612ab71cf6653c0454e7144
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-09-23 15:06:17 -0300

    feat: criando metodo logar de usuarioDao

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -1,5 +1,39 @@
 package dao;
 
-public class UsuarioDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import bd.Conexao;
+import models.Usuario;
+
+public class UsuarioDAO {
+	
+public Usuario Logar(String email, String senha) throws SQLException {
+		
+		String sql = "select id, nome, email, senha from usuarios " +
+		"where email = ? and senha = ? and ativo = true";
+		
+		try (Connection conexao = Conexao.obterConexao();
+				PreparedStatement logar = conexao.prepareStatement(sql)){
+				logar.setString(1, email);
+				logar.setString(2, senha);
+				
+				try(ResultSet resultado = logar.executeQuery()){
+					if(resultado.next()) {
+						Usuario usu = new Usuario();
+						usu.setId(resultado.getInt("id"));
+						usu.setNome(resultado.getString("nome"));
+						usu.setEmail(resultado.getString("email"));
+						usu.setPerfil(resultado.getString("perfil"));
+						
+						return usu;
+					}
+				}
+		}
+				
+		return null;
+		
+	}
 }
